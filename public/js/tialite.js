009c7dacb7da9b4fa1a38f07076f6065526514dc
Marcio Klepacz <marciok@Marcios-MacBook-Pro.local>
2012-10-25 11:59:05 -0200

    added loader

--- public/js/tialite.js
+++ public/js/tialite.js
@@ -16,19 +16,32 @@ var TiaLite = {
   login : function(form){
     $(form).ajaxForm({
       beforeSubmit : function(){
-        alert('validate')
+        // TODO: Make a validate funciton
+        var validate = true
+        if (validate) {
+          $('#login-area').fadeOut(function(){
+            $('#loader').fadeIn();
+          })
+        }
       },
       success : function(data){
+        if (data.grades == undefined) {
+          $('.alert p').text('Ops não foi possível achar as suas Notas :( ');
+          $('.alert').slideDown();
+        } else if (data.absences == undefined){
+          $('.alert p').text('Ops não foi possível achar as suas Faltas :( ');
+          $('.alert').slideDown();
+        }
         //Just a draft
-        $('#login-area').fadeOut(function(){
+        $('#loader').fadeOut(function(){
           $('#content-area').fadeIn(function(){
             $('.container').animate({
               width : 900
             },function(){
               $('#grades').html(data.grades);
               $('#absences').html(data.absences);
+              $('#grades, #absences').slideDown('slow');
               $('#grades, #absences').children('table').addClass('table table-striped')
-              
             })
           });
         })
