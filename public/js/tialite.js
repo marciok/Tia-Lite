803a949a3e61dba0228a424d2d5cf6bcf29bce99
Marcio Klepacz <marciok@Marcios-MacBook-Pro.local>
2012-11-04 01:26:24 -0200

    stuff did on the day

--- public/js/tialite.js
+++ public/js/tialite.js
@@ -1,77 +1,181 @@
 var TiaLite = {
   init : function(){
+    this.logout('.logout');
     this.login('.login-form');
+    this.refreshBt('.refresh');
+    this.gradesWeight('.grades-weight');
+
+    $('refresh-date').html(localStorage.getItem('time'));
+
+    $('.save-infos').attr('checked',true) 
+    if (localStorage.getItem('login')) {
+      $('.navbar').slideDown();
+
+      $('#grades').html(localStorage.getItem('grades'));
+      $('#absences').html(localStorage.getItem('absences'));
+      $('#schedule').html(localStorage.getItem('schedule'));
+
+      $('.container').css({ width : 900 })
+
+      $('#content-area').show();
+      
+      $('#tia').val(localStorage.getItem('tia'))
+      $('#pass').val(localStorage.getItem('pass'))
+
+
+      $('.login-form').submit();
+
+    } else {
+      $('#login-area').fadeIn();
+    }
   },
 
   helpers : {
     clearTableAttrs : function(table,attrs){
       $.each(attrs,function(i){
-        $(table).removeAttr(attrs[i]);
+        $(table+' *, '+table).removeAttr(attrs[i]);
       })
     },
 
-    validate : function(){
+    validate : function(fields){
+      $.each(fields,function(i){
+          _this = $(fields[i]);
+        if (_this.val() == ""){
+          _this.addClass('empty').attr('data-content','O campo '+_this.attr('placeholder')+' precisa ser preenchido').attr('data-title','Validação!');
+          _this.popover('show');
+        } else {
+          _this.removeClass('empty')
+        }
+      })
     }
 
   },
 
-  LSmanager : function(data){
-    // localStorage(data)
 
+  gradesWeight : function(bt){
+    $(bt).click(function(e){
+      e.preventDefault();
+      $('#grades table:nth-child(2)').slideToggle();
+    })
+  },
+
+  refreshBt : function(bt){
+    $(bt).click(function(e){
+      $(this).button('loading');
+      e.preventDefault();
+      $('.login-form').submit();
+    })
+  },
+
+  logout : function(bt){
+    $(bt).click(function(e){
+      e.preventDefault();
+      localStorage.clear();
+      location.reload();
+    })
   },
 
 
   login : function(form){
     $(form).ajaxForm({
       beforeSubmit : function(){
-        // TODO: Make a validate funciton
-        var validate = true
-        if (validate) {
-          $('#login-area').fadeOut(function(){
-            $('#loader').fadeIn();
-          })
+
+        TiaLite.helpers.validate(['#tia','#pass']);
+        if ($(form+' input').hasClass('empty')) {
+          return false;
         }
+
+        $('#login-area').fadeOut(function(){
+          $('#loader').fadeIn();
+        })
+
+        if ($('.save-infos').is(':checked')){
+          localStorage.setItem('login',true)
+        }
+
       },
       success : function(data){
-        // TODO: make this on server side
-        if (data.grades == undefined) {
-          $('.alert p').text('Ops não foi possível achar as suas Notas :( ');
-          $('.alert').slideDown();
-        } else if (data.absences == undefined){
-          $('.alert p').text('Ops não foi possível achar as suas Faltas :( ');
-          $('.alert').slideDown();
-        }
-        //Just a draft
-        $('#loader').fadeOut(function(){
-          $('#content-area').fadeIn(function(){
-            $('.container').animate({
-              width : 900
-            },function(){
-              $('#grades').html(data.grades);
-              $('#absences').html(data.absences);
+        if (data.status == "OK") {
+          
+          $('.refresh-date').html(data.time)
+          $('.refresh').button('reset')
+          // TODO: make this on server side
+          if (data.grades == undefined) {
+            $('.alert p').text('Ops não foi possível achar as suas Notas :( ');
+            $('.alert').slideDown();
+          } else if (data.absences == undefined){
+            $('.alert p').text('Ops não foi possível achar as suas Faltas :( ');
+            $('.alert').slideDown();
+          }
 
-              //Removing tables atributes;
-              var notWanted = ['id','width','cellpadding','cellspacing','align','colspan','width','class','bgcolor','height','tabelaemgeral']
-              //Absences
-              TiaLite.helpers.clearTableAttrs('#absences table',notWanted)
-              TiaLite.helpers.clearTableAttrs('#absences table tr td',notWanted)
-              TiaLite.helpers.clearTableAttrs('#absences table tr th',notWanted)
-              TiaLite.helpers.clearTableAttrs('#absences table tr td div',notWanted)
+          $('.navbar').slideDown();
+          
+
+        
+          //Just a draft
+          $('#loader').fadeOut(function(){
+            $('#content-area').fadeIn(function(){
+            $('refresh-date').html(data.time);
               
-              //Grades
-              TiaLite.helpers.clearTableAttrs('#grades table',notWanted)
-              TiaLite.helpers.clearTableAttrs('#grades table tr td',notWanted)
-              TiaLite.helpers.clearTableAttrs('#grades table tr td div',notWanted)
+              $('.container').animate({
+                width : 900
+              },function(){
+                $('#grades').html(data.grades);
+                $('#absences').html(data.absences);
+                $('#schedule').html(data.schedule);
 
-              $('#grades, #absences').slideDown('slow');
-              $('#grades, #absences').children('table').addClass('table table-striped')
-            })
+                //Removing tables atributes;
+                var notWanted = ['id','width','cellpadding','cellspacing','align','colspan','width','class','bgcolor','height','tabelaemgeral']
+                //Absences
+                TiaLite.helpers.clearTableAttrs('#absences table',notWanted);
+                
+                //Grades
+                TiaLite.helpers.clearTableAttrs('#grades table',notWanted);
+
+                //Schedule
+                TiaLite.helpers.clearTableAttrs('#schedule table',notWanted);
+
+                $('#schedule table').prepend('<tr class="period"><th colspan="7">');
+
+                $('#schedule table:first .period th').text('Manhã');
+                $('#schedule table:nth-child(2) .period th').text('Tarde');
+                $('#schedule table:nth-child(3) .period th').text('Noite');
+
+                $('#grades, #absences, #schedule').slideDown('slow');
+                $('#grades, #absences, #schedule').children('table').addClass('table table-striped')
+                
+
+                  //LocalStorage Setting
+                if (localStorage.getItem('login')) {
+
+                  localStorage.setItem('time',data.time)
+                  localStorage.setItem('tia',$('#tia').val())
+                  localStorage.setItem('pass',$('#pass').val())
+
+                  localStorage.setItem('grades',$('#grades').html())
+                  localStorage.setItem('absences',$('#absences').html())
+                  localStorage.setItem('schedule',$('#schedule').html())
+
+                }
+
+                
+
+              })
+            });
           });
-        })
+        } else {
+          $('#loader').hide();
+          $('#login-area').fadeIn();
+          $('.alert p').html(data.status);
+          $('.alert').fadeIn();
+        }
       },
 
       error : function(){
-        alert('error')
+        $('#login-area').fadeIn();
+        $('.alert p').html("Ocorreu algum erro com o sistema em todo em caso existe a <a href='www.mackenzie.com.br'>maneira tradicional</a>");
+        $('.alert').fadeIn();
+        
       }
     })
   }
--- tialite.rb
+++ tialite.rb
@@ -13,28 +13,41 @@ class TiaLite < Sinatra::Application
   # Posting all the infos
   post "/fetch-my-tia" do
     content_type :json
+    if params[:tia].empty? || params[:pass].empty?
+      return {:message => "Tia e Senha sao obrigatorios"}.to_json
+    end
 
     a = Mechanize.new
     a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    a.get(settings.urls[:login]) do |page|
+    begin
+      a.get(settings.urls[:login]) do |page|
 
-      # Submit the login form
-      @tia_main_p = page.form_with(:action => 'verifica.php') do |f|
-        f.alumat  = params[:tia]
-        f.pass    = params[:pass]
-      end.click_button
+        # Submit the login form
+        @tia_main_p = page.form_with(:action => 'verifica.php') do |f|
+          f.alumat  = params[:tia]
+          f.pass    = params[:pass]
+        end.click_button
 
-      @absence_p = @tia_main_p.link_with(:href => settings.urls[:absences]).click
-      @grades_p = @tia_main_p.link_with(:href => settings.urls[:grades]).click
+        if @tia_main_p.uri.to_s == settings.urls[:error]
+          return {:status => "Confira o seu usuario e senha"}.to_json
+        end
 
-      #geting all the infos:
-      @abs = @absence_p.search(".#{settings.targets[:absences]}")
-      @grds = @grades_p.search("##{settings.targets[:grades]}")
+        @absence_p = @tia_main_p.link_with(:href => settings.urls[:absences]).click
+        @grades_p = @tia_main_p.link_with(:href => settings.urls[:grades]).click
+        @schedule_p = @tia_main_p.link_with(:href => settings.urls[:schedule]).click
 
-    end
+        # Geting all the infos:
+        @abs = @absence_p.search(".#{settings.targets[:absences]}")
+        @grds = @grades_p.search("##{settings.targets[:grades]}")
+        @sch = @schedule_p.search("##{settings.targets[:grades]}")
 
+      end
+    rescue Net::HTTP::Persistent::Error
+      return {:status => "Aguarde um tempo o sistema esta sendo recarregado"}.to_json
+    end  
+    t = DateTime.now
 
-    return {:absences => @abs, :grades => @grds}.to_json
+    return {:status => "OK",:absences => @abs, :grades => @grds, :schedule => @sch, :time => t.strftime('%a, %d %b %Y %H:%M:%S')}.to_json
 
   end
 
